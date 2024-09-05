e6b5c95252bdfcd16d2061152d91458bb34637f3
Ali <alinksht@gmail.com>
2024-09-05 21:27:36 +0200

    add contact us service

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
-const page = () => {
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    from_name: '',
+    reply_to: '',
+    subject: '',
+    message: '',
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    emailjs.send('service_0zqifmo', 'template_y22vs43', formData, 'lG3AmMgXExhdJmOID')
+      .then((result) => {
+        console.log(result.text);
+        alert('Message sent successfully!');
+      })
+      .catch((error) => {
+        console.log(error.text);
+        alert('Failed to send message, please try again later.');
+      });
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+          Got a technical issue? Let us know.
+        </p>
+        <form onSubmit={sendEmail} className="space-y-8">
+          <div>
+            <label htmlFor="from_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
+            <input
+              type="text"
+              id="from_name"
+              name="from_name"
+              onChange={handleInputChange}
+              className="shadow-sm bg-gray-50 text-sm rounded-lg block w-full p-2.5"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="reply_to" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Email</label>
+            <input
+              type="email"
+              id="reply_to"
+              name="reply_to"
+              onChange={handleInputChange}
+              className="shadow-sm bg-gray-50 text-sm rounded-lg block w-full p-2.5"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              onChange={handleInputChange}
+              className="shadow-sm bg-gray-50 text-sm rounded-lg block w-full p-2.5"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              rows={6}
+              onChange={handleInputChange}
+              className="shadow-sm bg-gray-50 text-sm rounded-lg block w-full p-2.5"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="py-3 px-5 text-sm font-medium text-white rounded-lg bg-primary-700"
+          >
+            Send message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default Contact;
--- src/app/historical-periods/page.tsx
+++ src/app/historical-periods/page.tsx
@@ -1,69 +1,91 @@
-'use client';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import Spinner from '@/components/ui/Spinner';
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "@/components/ui/Spinner";
 
 // Type definitions
 interface Article {
-    title: string;
-    url: string;
+  title: string;
+  url: string;
 }
 
 interface Period {
-    id: number;
-    title: string;
-    description: string;
-    imageUrl: string;
-    articles: Article[];
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  articles: Article[];
 }
 
 // Function to fetch data
 const fetchPeriods = async (): Promise<Period[]> => {
-    const response = await fetch('/data/historicalPeriods.json');
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
+  const response = await fetch("/data/historicalPeriods.json");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
 };
 
 // Client component to display historical periods
 const HistoricalPeriods: React.FC = () => {
-    const { data: periods, error, isLoading } = useQuery<Period[], Error>({
-        queryKey: ['periods'],
-        queryFn: fetchPeriods
-    });
+  const {
+    data: periods,
+    error,
+    isLoading,
+  } = useQuery<Period[], Error>({
+    queryKey: ["periods"],
+    queryFn: fetchPeriods,
+  });
 
-    if (isLoading) return <Spinner />;
-    if (error) return <p>Error fetching historical periods: {error.message}</p>;
+  if (isLoading) return <Spinner />;
+  if (error) return <p>Error fetching historical periods: {error.message}</p>;
 
-    return (
-        <div className="container mx-auto p-4">
-            <h1 className="text-4xl font-bold mb-6 text-center text-accent">Historical Periods</h1>
-            <p className="text-center text-gray-600 mb-6">
-                Explore the major historical periods of Iran, from ancient times through the Islamic era and into the modern age. Each period is rich with its own events, significant figures, and cultural developments.
-            </p>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                {periods?.map((period) => (
-                    <div key={period.id} className="bg-white shadow-md rounded-lg overflow-hidden">
-                        <Image style={{ width: "100%", height: "auto" }} width={250} height={150} objectFit='cover' src={period.imageUrl} alt={period.title} />
-                        <div className="p-4">
-                            <h2 className="text-2xl font-semibold mb-2">{period.title}</h2>
-                            <p className="text-gray-700 mb-4">{period.description}</p>
-                            <div className="space-y-2">
-                                {period.articles.map((article, index) => (
-                                    <Link key={index} href={article.url} className="text-blue-500 hover:underline">
-                                        {article.title}
-                                    </Link>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center p-4">
+      <h1 className="mb-6 text-center text-5xl font-bold text-accent">
+        Historical Periods
+      </h1>
+      <p className="mb-6 w-[55rem] text-center text-gray-600">
+        Explore the major historical periods of Iran, from ancient times through
+        the Islamic era and into the modern age. Each period is rich with its
+        own events, significant figures, and cultural developments.
+      </p>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
+        {periods?.map((period) => (
+          <div
+            key={period.id}
+            className="overflow-hidden rounded-lg bg-[#fff] h-[25rem] shadow-md flex flex-col justify-between"
+          >
+            <Image
+              style={{ width: "100%", height: "auto" }}
+              width={250}
+              height={150}
+              objectFit="cover"
+              src={period.imageUrl}
+              alt={period.title}
+            />
+            <div className="p-4">
+              <h2 className="mb-2 text-3xl font-semibold">{period.title}</h2>
+              <p className="mb-4 text-gray-700">{period.description}</p>
+              <div className="space-y-2">
+                {period.articles.map((article, index) => (
+                  <Link
+                    key={index}
+                    href={article.url}
+                    className="text-blue-500 hover:underline"
+                  >
+                    {article.title}
+                  </Link>
                 ))}
+              </div>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HistoricalPeriods;
